900a769c103b29c2ab398506687cb80ea79471b9
Jonathan Raoult <jesuisjonathan@gmx.fr>
2015-05-12 20:14:44 +1000

    reduce the audio latency by reducing the audio "ptime"

--- app/sdpTransformations.js
+++ app/sdpTransformations.js
@@ -5,8 +5,8 @@ var sdpTransform = require('sdp-transform');
 module.exports = {
   tranform: function(sdp) {
     var sdpObj = sdpTransform.parse(sdp);
-    sdpObj.media[0].fmtp[0].config = 'minptime=3; stereo=1;';
-    sdpObj.media[0].maxptime = 5;
+    // turn on stereo and reduce packet time for opus codec (minimum seems to be 10ms)
+    sdpObj.media[0].fmtp[0].config = 'ptime=10; stereo=1;';
     var output = sdpTransform.write(sdpObj);
     return output;
   }
