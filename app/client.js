c64588f924865603085e7ebd67df3e0d2a9da39e
Jonathan Raoult <jesuisjonathan@gmx.fr>
2015-05-15 18:16:38 +1000

    - made sure the "stream" event listener is registered soon enough

--- app/client.js
+++ app/client.js
@@ -13,10 +13,10 @@ function listMidiInputs() {
 
 /**
  *
- * @param {function(SimplePeer)} cb we use a callback here because we need the synchronous nature
+ * @param {function(SimplePeer)} init we use a callback here because we need the synchronous nature
  * of it to make sure event listeners are regitered before any event is triggered
  */
-function connect(cb) {
+function connect(init) {
   var reception = channelFactory('reception'),
     channel = channelFactory(),
     simplePeer;
@@ -27,40 +27,45 @@ function connect(cb) {
     })
     .subscribe()
     .then(function whenSubscribed() {
-
       console.log('Client listening to signals from server on channel %s', channel.id);
 
-      simplePeer = new SimplePeer({
-        sdpTransform: sdpTransform
-      });
+      return new Promise(function(resolve, reject) {
 
-      return reception
-        .publish(channel.id)
-        .then(function() {
-
-          simplePeer.on('error', console.error);
-
-          simplePeer.on('signal', function(data) {
-            channel
-              .publish(data)
-              .catch(console.error);
-          });
-
-          simplePeer.on('close', function() {
-            simplePeer.destroy();
-            channel.destroy();
-          });
-
-          cb(simplePeer);
+        simplePeer = new SimplePeer({
+          sdpTransform: sdpTransform
         });
+
+        simplePeer.on('error', reject);
+
+        simplePeer.on('connect', function() {
+          console.log('New peer to peer connection with the server ready');
+          resolve();
+        });
+
+        init(simplePeer);
+
+        reception
+          .publish(channel.id)
+          .then(function() {
+            simplePeer.on('signal', function(data) {
+              channel
+                .publish(data)
+                .catch(console.error);
+            });
+
+            simplePeer.on('close', function() {
+              simplePeer.destroy();
+              channel.destroy();
+            });
+          })
+          .catch(reject);
+      });
     });
 }
 
 function start(midiInput) {
 
-  return connect(function whenConnected(simplePeer) {
-
-    console.log('New peer to peer connection with the server ready');
+  return connect(function init(simplePeer) {
 
     simplePeer.on('stream', function(remoteStream) {
       var player = new Audio();
